70f22b65f8d9371a8951ca38a2c1d5077a15acd9
Semen_Maksimovich <57765727+Laki-man3@users.noreply.github.com>
2026-03-16 20:24:36 +0700

    Add files via upload

---
+++ JS/sw.js
@@ -0,0 +1,43 @@
+const CACHE='qa-v1';
+const ASSETS=[
+  './',
+  './index.html',
+  './CSS/style.css',
+  './CSS/effects.css',
+  './JS/main.js',
+  './JS/effects.js'
+];
+
+self.addEventListener('install',function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){return c.addAll(ASSETS);})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',function(e){
+  if(e.request.method!=='GET')return;
+  const url=new URL(e.request.url);
+  if(url.origin!==location.origin)return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      const net=fetch(e.request).then(function(res){
+        if(res&&res.status===200){
+          const clone=res.clone();
+          caches.open(CACHE).then(function(c){c.put(e.request,clone);});
+        }
+        return res;
+      }).catch(function(){});
+      return cached||net;
+    })
+  );
+});
